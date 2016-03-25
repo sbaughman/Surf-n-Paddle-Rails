327861a96ba2b14935143c684c807ac4b7ac1b43
Seth Baughman <sbaughman1@me.com>
2016-03-25 10:19:54 -0400

    Refactored post-links at bottom of page. Add author images

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,13 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.sort_by(&:created_at)
-    @newest_post = @posts.first
+    @current_post = @posts.first
   end
 
   def show
+    @posts = Post.all.sort_by(&:created_at)
+    @current_post = @posts.detect { |post| post.id == params[:id].to_i }
+    render 'index'
   end
 
 end
