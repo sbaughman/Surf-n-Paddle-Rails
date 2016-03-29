4d31f83fe01b50b098f4fd2984a7487c63d7dd00
Seth Baughman <sbaughman1@me.com>
2016-03-29 15:12:15 -0400

    Add relationship between authors and posts and modify index.html

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160329182405) do
+ActiveRecord::Schema.define(version: 20160329190229) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -23,11 +23,13 @@ ActiveRecord::Schema.define(version: 20160329182405) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.string   "author"
     t.string   "thumbnail"
     t.string   "interject"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "author_id"
   end
+
+  add_index "posts", ["author_id"], name: "index_posts_on_author_id"
 
 end
