d600d43c96ea271ce0bbc249892bded0ce356f14
Seth Baughman <sbaughman1@me.com>
2016-03-29 13:58:27 -0400

    Add posts to database

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,15 @@
 class Post < ActiveRecord::Base
 
+  def first_half(text)
+    paragraph_array = text.split("\n")
+    first_half = paragraph_array.slice(0..paragraph_array.length/2 - 1)
+    first_half.join("\n")
+  end
 
+  def second_half(text)
+    paragraph_array = text.split("\n")
+    second_half = paragraph_array.slice(paragraph_array.length/2..-1)
+    second_half.join("\n")
+  end
 
 end
